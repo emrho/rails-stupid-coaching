780a72d920676fa2628e18bb4975dd5fd6f9080c
Emily Rhodes <emilyerhodes@hotmail.com>
2018-08-06 14:27:51 +0200

    coaching app working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'questions/ask'
+  get 'ask', to: 'questions#ask', as: :ask
+  get 'questions/answer'
+  get 'answer', to: 'questions#answer', as: :answer
+  post 'question', to: 'question#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
